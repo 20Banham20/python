0d1e0b368ff611d13d5cd88a4f420803c459ad65
20banham20 <jb22370@my.westlake.school.nz>
2024-08-21 21:09:12 +1200

    commit

--- atm.py
+++ atm.py
@@ -12,19 +12,31 @@ def transaction_history():
 
 pin=8967
 x= True
-attemps = 3
+attemps = 4
 
-while x != False:
-    user_input = int(input("Please insert the pin:"))
-    if user_input == pin:
-        print("welcome to the atm\n")
+while x == True:
+    user_pin_input = int(input("Please insert the pin:"))
+    if user_pin_input == pin:
+        print("Welcome to the ATM!\n")
+        print("Please pick what you would like to do!")
+        print(" 1.Check balance\n 2.Deposit\n 3.Withdraw\n 4.Transaction history\n 5.Exit")
+    
+        user_input= int(input("Please enter the number corresponding to your action:"))
+    elif user_input == 1:
+        check_balance()
+    elif user_input == 2:
+        withdraw()
+    elif user_input == 3:
+        deposit()
+    elif user_input == 4:
+        transaction_history()
+    elif user_input == 5:
+        break
     else:
-        attemps - 1
-        if attemps >= 0:
-            
-            print ("You have ")
-
+        attemps -= 1
+        if attemps >= 1:
+            print (f"You have {attemps} left")
         else:
-            attemps
+            break
 
             
